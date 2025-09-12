be63bbaf180ee4ab5c490cf061f2214f20d14d1b
iDi <ideaidiot1729@gmail.com>
2025-09-12 14:35:07 +0900

    ac

--- abc076b.cpp
+++ abc076b.cpp
@@ -1,1 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int N, K;
+    cin >> N >> K;
+    int A = 1;
+    for (int i = 0; i < N; i++) {
+        if (A*2 < A+K) {
+            A *= 2;
+        } else {
+            A += K;
+        }
+    }
+    cout << A << endl;
+}
